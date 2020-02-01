dd921e2cde296b1736ea1badc507e33bd1f83770
Emiliano Montenegro <emm.montenegro@gmail.com>
2020-02-01 11:21:08 -0300

    minor changes

--- GlobalGameJamGame/characters/Turtle.gd
+++ GlobalGameJamGame/characters/Turtle.gd
@@ -7,5 +7,12 @@ export (PackedScene) var WeaponRight
 onready var game_timer: Timer = owner.get_node('GameTimer')
 
 func _ready():
-    game_timer.connect('timeout', self, '_on_PoisonTimer_timeout')
+    game_timer.connect('timeout', self, '_on_Game_timeout')
+    pass
 
+func _on_Game_timeout() -> void:
+    
+    pass
+
+func change_weapon(weapon: PackedScene, position: int) -> void:
+    pass
