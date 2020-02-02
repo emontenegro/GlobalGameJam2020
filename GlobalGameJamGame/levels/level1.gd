b8d1eebb39ed729af700922ceb13d1ee7aff8598
Zevbel <sebaguero9@gmail.com>
2020-02-01 22:44:41 -0300

    Merge
    Merge

--- GlobalGameJamGame/levels/level1.gd
+++ GlobalGameJamGame/levels/level1.gd
@@ -26,45 +26,17 @@ signal p1wchanged
 signal p2wchanged
 
 func _ready():
-    $Arena/Player1.connect("player_dies", self, '_end_game')
-    $Arena/Player2.connect("player_dies", self, '_end_game')
-    $AnimationPlayer.play("Start_Game")
-    pass
 	$Arena/Player1.connect("player_dies", self, '_end_game')
 	$Arena/Player2.connect("player_dies", self, '_end_game')
 	$AnimationPlayer.play("Start_Game")
 	pass
 
 func start_game():
-    print('start game')
-    emit_signal("startgame")
-    $GameTimer.start()
 	print('start game')
 	emit_signal("startgame")
 	$GameTimer.start()
 
 func _input(event):
-    if event.is_action_pressed('p1w1r'):
-        emit_signal("p1stop")
-        emit_signal('p1w1r')
-    if event.is_action_pressed('p1w2r'):
-        emit_signal("p1stop")
-        emit_signal('p1w2r')
-    if event.is_action_pressed('p1w3r'):
-        emit_signal("p1stop")
-        emit_signal('p1w3r')
-    if event.is_action_pressed('p1w1c'):
-        emit_signal("p1stop")
-        emit_signal("p1wchanged")
-        emit_signal('p1w1c')
-    if event.is_action_pressed('p1w2c'):
-        emit_signal("p1stop")
-        emit_signal("p1wchanged")
-        emit_signal('p1w2c')
-    if event.is_action_pressed('p1w3c'):
-        emit_signal("p1stop")
-        emit_signal("p1wchanged")
-        emit_signal('p1w3c')
 	if event.is_action_pressed('p1w1r'):
 		emit_signal("p1stop")
 		emit_signal('p1w1r')
@@ -87,27 +59,6 @@ func _input(event):
 		emit_signal("p1wchanged")
 		emit_signal('p1w3c')
 
-    if event.is_action_pressed('p2w1r'):
-        emit_signal("p2stop")
-        emit_signal('p2w1r')
-    if event.is_action_pressed('p2w2r'):
-        emit_signal("p2stop")
-        emit_signal('p2w2r')
-    if event.is_action_pressed('p2w3r'):
-        emit_signal("p2stop")
-        emit_signal('p2w3r')
-    if event.is_action_pressed('p2w1c'):
-        emit_signal("p2stop")
-        emit_signal("p2wchanged")
-        emit_signal('p2w1c')
-    if event.is_action_pressed('p2w2c'):
-        emit_signal("p2stop")
-        emit_signal("p2wchanged")
-        emit_signal('p2w2c')
-    if event.is_action_pressed('p2w3c'):
-        emit_signal("p2stop")
-        emit_signal("p2wchanged")
-        emit_signal('p2w3c')
 	if event.is_action_pressed('p2w1r'):
 		emit_signal("p2stop")
 		emit_signal('p2w1r')
@@ -131,17 +82,11 @@ func _input(event):
 		emit_signal('p2w3c')
 
 func _on_GameTimer_timeout():
-    emit_signal("p1damage", 10)
-    emit_signal("p2damage", 10)
-    pass
 	emit_signal("p1damage", $Arena/Player2.current_damage)
 	emit_signal("p2damage", $Arena/Player1.current_damage)
 	pass
 
 func _end_game(player: int):
-    emit_signal("endgame")
-    $GameTimer.stop()
-    print('player: '+str(player)+' died.')
 	emit_signal("endgame")
 	$GameTimer.stop()
 	print('player: '+str(player)+' died.')
--- GlobalGameJamGame/weapons/WeaponButtonBase.gd
+++ GlobalGameJamGame/weapons/WeaponButtonBase.gd
@@ -5,7 +5,6 @@ signal repair_weapon
 
 export (int) var Player = 1
 export (int) var Weapon = 1
-export (int) var Repair_Timeout = 0
 export (int) var Repair = 0
 export (int) var Damage = 0
 export (int) var Spoil = 0
@@ -19,15 +18,6 @@ var is_active =  false
 var is_destroyed = false
 
 func _ready():
-    var ss = 'p'+str(Player)+'w'+str(Weapon)
-    owner.connect(ss+'r', self, '_on_repair')
-    owner.connect(ss+'c', self, '_on_change')
-    owner.connect('p'+str(Player)+'stop', self, '_on_stop_repair')
-    owner.connect('p'+str(Player)+'damage', self, '_on_take_damage')
-    owner.connect('p'+str(Player)+'wchanged', self, '_on_weapon_changed')
-    
-    current_health = 100
-    $Texture/HealthProgress.value = current_health
 	var ss = 'p'+str(Player)+'w'+str(Weapon)
 	owner.connect(ss+'r', self, '_on_repair')
 	owner.connect(ss+'c', self, '_on_change')
@@ -40,12 +30,6 @@ func _ready():
 
 
 func _on_repair():
-    if $Timer.is_stopped():
-        is_repearing = true
-        
-    if is_repearing and $Timer.is_stopped():
-        $Timer.start()
-    pass
 	if $Timer.is_stopped():
 		is_repearing = true
 		
@@ -54,20 +38,12 @@ func _on_repair():
 	pass
 
 func _on_timeout():
-    if !is_destroyed:
-        do_repair()
-    pass
 	if !is_destroyed:
 		do_repair()
 	pass
 
 
 func do_repair() -> void:
-    var new_health = current_health + Repair_Timeout
-    current_health = clamp(new_health, 0, 100)
-    $Texture/HealthProgress.value = current_health
-    emit_signal("repair_weapon", WeaponName, current_health, Player)
-    print_debug('Repair Weapon: '+ str(WeaponName) +' Curr: '+ str(current_health))
 	var new_health = current_health + Repair
 	current_health = clamp(new_health, 0, 100)
 	$Texture/HealthProgress.value = current_health
@@ -75,11 +51,6 @@ func do_repair() -> void:
 	print_debug('Repair Weapon: '+ str(WeaponName) +' Curr: '+ str(current_health))
 
 func _on_change():
-    if !is_destroyed:
-        stop_repair()
-        is_active =  true
-        emit_signal("change_weapon", WeaponName, Player) 
-        print_debug('Change Weapon: '+ str(WeaponName) +' Curr: '+ str(current_health))
 	if !is_destroyed:
 		stop_repair()
 		is_active =  true
@@ -88,31 +59,17 @@ func _on_change():
 
 
 func _on_stop_repair():
-    stop_repair()
 	stop_repair()
 
 func stop_repair():
-    is_repearing = false
-    if !$Timer.is_stopped():
-        $'Timer'.stop()
-        
 	is_repearing = false
 	if !$Timer.is_stopped():
 		$'Timer'.stop()
 		
 func _on_weapon_changed():
-    is_active = false
-    
 	is_active = false
 	
 func _on_take_damage(damage: int):
-    if is_active:
-        var new_health = current_health - damage
-        current_health = clamp(new_health, 0, 100)
-        $Texture/HealthProgress.value = current_health
-        if current_health == 0:
-            is_destroyed = true
-            emit_signal("change_weapon", '', Player) 
 	if is_active:
 		var new_health = current_health - Spoil
 		current_health = clamp(new_health, 0, 100)
