d6058c43f1062d5556009b49c803200f93bf42c7
Emiliano Montenegro <emm.montenegro@gmail.com>
2020-02-01 23:14:45 -0300

    Possible Wrong

--- GlobalGameJamGame/characters/Turtle.gd
+++ GlobalGameJamGame/characters/Turtle.gd
@@ -11,56 +11,56 @@ var current_health: int
 var is_finished = false
 
 func _ready():
-	current_damage = 1
-	current_health = 100
-	$HealthBar.value = current_health
-	game_timer.connect('timeout', self, '_on_Game_timeout')
-	owner.connect('p'+str(player)+'damage', self, '_on_Level1_player_hit')
-	owner.connect('startgame', self, '_start_game')
-	owner.connect('endgame', self, '_end_game')
-	if player == 1:
-		for state_node in owner.get_node('PlayerOneToolbox').get_children():
-			state_node.connect("change_weapon", self, "_change_weapon")
-	if player == 2:
-		for state_node in owner.get_node('PlayerTwoToolbox').get_children():
-			state_node.connect("change_weapon", self, "_change_weapon")
-			
-	hide_all_weapon()
-	pass
-	
+    current_damage = 1
+    current_health = 100
+    $HealthBar.value = current_health
+    game_timer.connect('timeout', self, '_on_Game_timeout')
+    owner.connect('p'+str(player)+'damage', self, '_on_Level1_player_hit')
+    owner.connect('startgame', self, '_start_game')
+    owner.connect('endgame', self, '_end_game')
+    if player == 1:
+        for state_node in owner.get_node('PlayerOneToolbox').get_children():
+            state_node.connect("change_weapon", self, "_change_weapon")
+    if player == 2:
+        for state_node in owner.get_node('PlayerTwoToolbox').get_children():
+            state_node.connect("change_weapon", self, "_change_weapon")
+            
+    hide_all_weapon()
+    pass
+    
 func _start_game():
-	$Body/AnimationPlayer.play("Attack")    
+    $Body/AnimationPlayer.play("Attack")    
 
 func _end_game():
-	$Body/AnimationPlayer.play("idle")
-	is_finished = true
+    $Body/AnimationPlayer.play("idle")
+    is_finished = true
 
 func _on_Game_timeout() -> void:
-	
-	pass
-	
+    
+    pass
+    
 func hide_all_weapon():
-	for node in get_node("Body/ArmR/Weapon").get_children():
-		node.hide()
+    for node in get_node("Body/ArmR/Weapon").get_children():
+        node.hide()
 
 func _change_weapon(weapon: String, player: int, damage: int) -> void:
-	current_damage = damage
-	if !is_finished:
-		hide_all_weapon()
-		if player == self.player:
-			if weapon != '':
-				get_node("Body/ArmR/Weapon/"+weapon).show()
-				$Body/AnimationPlayer.play("SwapWeapon")
-				yield($Body/AnimationPlayer,"animation_finished")
-				$Body/AnimationPlayer.play("Attack")  
-	pass
+    current_damage = damage
+    if !is_finished:
+        hide_all_weapon()
+        if player == self.player:
+            if weapon != '':
+                get_node("Body/ArmR/Weapon/"+weapon).show()
+                $Body/AnimationPlayer.play("SwapWeapon")
+                yield($Body/AnimationPlayer,"animation_finished")
+                $Body/AnimationPlayer.play("Attack")  
+    pass
 
 
 func _on_Level1_player_hit(damage: int):
-	current_health = current_health - damage
-	current_health = clamp(current_health, 0, 100)
-	$HealthBar.value = current_health
-	print(current_health)
-	if current_health <= 0:
-		emit_signal("player_dies", player)
-	pass
+    current_health = current_health - damage
+    current_health = clamp(current_health, 0, 100)
+    $HealthBar.value = current_health
+    print(current_health)
+    if current_health <= 0:
+        emit_signal("player_dies", player)
+    pass
--- GlobalGameJamGame/weapons/WeaponButtonBase.gd
+++ GlobalGameJamGame/weapons/WeaponButtonBase.gd
@@ -18,62 +18,62 @@ var is_active =  false
 var is_destroyed = false
 
 func _ready():
-	var ss = 'p'+str(Player)+'w'+str(Weapon)
-	owner.connect(ss+'r', self, '_on_repair')
-	owner.connect(ss+'c', self, '_on_change')
-	owner.connect('p'+str(Player)+'stop', self, '_on_stop_repair')
-	owner.connect('p'+str(Player)+'damage', self, '_on_take_damage')
-	owner.connect('p'+str(Player)+'wchanged', self, '_on_weapon_changed')
-	
-	current_health = 100
-	$Texture/HealthProgress.value = current_health
+    var ss = 'p'+str(Player)+'w'+str(Weapon)
+    owner.connect(ss+'r', self, '_on_repair')
+    owner.connect(ss+'c', self, '_on_change')
+    owner.connect('p'+str(Player)+'stop', self, '_on_stop_repair')
+    owner.connect('p'+str(Player)+'damage', self, '_on_take_damage')
+    owner.connect('p'+str(Player)+'wchanged', self, '_on_weapon_changed')
+    
+    current_health = 100
+    $Texture/HealthProgress.value = current_health
 
 
 func _on_repair():
-	if $Timer.is_stopped():
-		is_repearing = true
-		
-	if is_repearing and $Timer.is_stopped():
-		$Timer.start()
-	pass
+    if $Timer.is_stopped():
+        is_repearing = true
+        
+    if is_repearing and $Timer.is_stopped():
+        $Timer.start()
+    pass
 
 func _on_timeout():
-	if !is_destroyed:
-		do_repair()
-	pass
+    if !is_destroyed:
+        do_repair()
+    pass
 
 
 func do_repair() -> void:
-	var new_health = current_health + Repair
-	current_health = clamp(new_health, 0, 100)
-	$Texture/HealthProgress.value = current_health
-	emit_signal("repair_weapon", WeaponName, current_health, Player)
-	print_debug('Repair Weapon: '+ str(WeaponName) +' Curr: '+ str(current_health))
+    var new_health = current_health + Repair
+    current_health = clamp(new_health, 0, 100)
+    $Texture/HealthProgress.value = current_health
+    emit_signal("repair_weapon", WeaponName, current_health, Player)
+    print_debug('Repair Weapon: '+ str(WeaponName) +' Curr: '+ str(current_health))
 
 func _on_change():
-	if !is_destroyed:
-		stop_repair()
-		is_active =  true
-		emit_signal("change_weapon", WeaponName, Player, Damage) 
-		print_debug('Change Weapon: '+ str(WeaponName) +' Curr: '+ str(current_health))
+    if !is_destroyed:
+        stop_repair()
+        is_active =  true
+        emit_signal("change_weapon", WeaponName, Player, Damage) 
+        print_debug('Change Weapon: '+ str(WeaponName) +' Curr: '+ str(current_health))
 
 
 func _on_stop_repair():
-	stop_repair()
+    stop_repair()
 
 func stop_repair():
-	is_repearing = false
-	if !$Timer.is_stopped():
-		$'Timer'.stop()
-		
+    is_repearing = false
+    if !$Timer.is_stopped():
+        $'Timer'.stop()
+        
 func _on_weapon_changed():
-	is_active = false
-	
+    is_active = false
+    
 func _on_take_damage(damage: int):
-	if is_active:
-		var new_health = current_health - Spoil
-		current_health = clamp(new_health, 0, 100)
-		$Texture/HealthProgress.value = current_health
-		if current_health == 0:
-			is_destroyed = true
-			emit_signal("change_weapon", '', Player, 1) 
+    if is_active:
+        var new_health = current_health - Spoil
+        current_health = clamp(new_health, 0, 100)
+        $Texture/HealthProgress.value = current_health
+        if current_health == 0:
+            is_destroyed = true
+            emit_signal("change_weapon", '', Player, 1) 
